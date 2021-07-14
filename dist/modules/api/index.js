e6378007e2e6dc7ef2bf03a9843740c248c94b01
unknown <alan.349@hotmail.com>
2021-07-14 15:34:16 -0300

    prettified

--- dist/modules/api/index.js
+++ dist/modules/api/index.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 const API = axios.create({
-    baseURL: "https://api.trakt.tv/",
-    headers: {
-        "Content-Type": "application/json",
-        "trakt-api-version": "2",
-        "trakt-api-key": "6849011413a1d64a578f43202d13b2263c98e5b6a6240a46c7b670a070807c52",
-    },
+  baseURL: "https://api.trakt.tv/",
+  headers: {
+    "Content-Type": "application/json",
+    "trakt-api-version": "2",
+    "trakt-api-key":
+      "6849011413a1d64a578f43202d13b2263c98e5b6a6240a46c7b670a070807c52",
+  },
 });
 export { API };
--- dist/modules/api/summaryMovies.js
+++ dist/modules/api/summaryMovies.js
@@ -3,18 +3,27 @@ import humanizeDuration from "humanize-duration";
 import { summaryMovieMenu } from "../menu";
 import { log } from "../log";
 const summaryMovie = (movie, list, origin) => {
-    console.clear();
-    log(chalk.bold.italic("Sobre o Filme: \n"));
-    log(`Título: ${chalk.bold(movie.title)}`);
-    log(`Data de Lançamento: ${chalk.bold(new Date(movie.released).toLocaleDateString())}`);
-    log(`Classificação Indicativa: ${chalk.bold(movie.certification)}`);
-    log(`Duração do Filme: ${chalk.bold(humanizeDuration(movie.runtime * 60000, { language: "pt" }))}`);
-    log(`Nota do Filme: ${chalk.bold(movie.rating)}`);
-    log(`Gêneros: ${chalk.bold(movie.genres.join(", "))}`);
-    summaryMovieMenu(list, origin);
+  console.clear();
+  log(chalk.bold.italic("Sobre o Filme: \n"));
+  log(`Título: ${chalk.bold(movie.title)}`);
+  log(
+    `Data de Lançamento: ${chalk.bold(
+      new Date(movie.released).toLocaleDateString()
+    )}`
+  );
+  log(`Classificação Indicativa: ${chalk.bold(movie.certification)}`);
+  log(
+    `Duração do Filme: ${chalk.bold(
+      humanizeDuration(movie.runtime * 60000, { language: "pt" })
+    )}`
+  );
+  log(`Nota do Filme: ${chalk.bold(movie.rating)}`);
+  log(`Gêneros: ${chalk.bold(movie.genres.join(", "))}`);
+  summaryMovieMenu(list, origin);
 };
 const summaryByListPosition = (list, origin, position) => {
-    const movie = origin == "popularMovies" ? list[position - 1] : list[position - 1].movie;
-    summaryMovie(movie, list, origin);
+  const movie =
+    origin == "popularMovies" ? list[position - 1] : list[position - 1].movie;
+  summaryMovie(movie, list, origin);
 };
 export { summaryByListPosition };
