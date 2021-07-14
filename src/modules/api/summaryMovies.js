b8e0633e3bd543103a6972cd335017dcdc8dc94a
unknown <alan.349@hotmail.com>
2021-07-13 22:01:18 -0300

    improved summary

--- src/modules/api/summaryMovies.js
+++ src/modules/api/summaryMovies.js
@@ -1,21 +1,17 @@
 import humanizeDuration from "humanize-duration";
-// import { API } from "./index.js";
+import { summaryMovieMenu } from "../menu";
 import { log, chalk } from "../../log";
 
-const summaryMovie = (movie) => {
+const summaryMovie = (movie, list, origin) => {
   console.clear();
-  log(chalk.bold.italic("Tudo Sobre o Filme: \n"));
+  log(chalk.bold.italic("Sobre o Filme: \n"));
   log(`Título: ${chalk.bold(movie.title)}`);
   log(
     `Data de Lançamento: ${chalk.bold(
       new Date(movie.released).toLocaleDateString()
     )}`
   );
-  log(
-    `Classificação Indicativa: ${chalk.bold(
-      humanizeDuration(movie.certification)
-    )}`
-  );
+  log(`Classificação Indicativa: ${chalk.bold(movie.certification)}`);
   log(
     `Duração do Filme: ${chalk.bold(
       humanizeDuration(movie.runtime * 60000, { language: "pt" })
@@ -23,12 +19,14 @@ const summaryMovie = (movie) => {
   );
   log(`Nota do Filme: ${chalk.bold(movie.rating)}`);
   log(`Gêneros: ${chalk.bold(movie.genres.join(", "))}`);
+
+  summaryMovieMenu(list, origin);
 };
 
 const summaryByListPosition = (list, origin, position) => {
   const movie =
     origin == "popularMovies" ? list[position - 1] : list[position - 1].movie;
-  summaryMovie(movie);
+  summaryMovie(movie, list, origin);
 };
 
 export { summaryByListPosition };
